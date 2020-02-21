1f895110b9fea3944aa788a225507b8f5f619830
Laura <burbach@comm.rwth-aachen.de>
2020-02-21 16:27:16 +0100

    project stance

--- data_conversion.R
+++ data_conversion.R
@@ -10,8 +10,40 @@ limit_size <- 10
 # Where to load the data from
 input_file <- "data/BoundedRationality All-table.csv"
 
+skip_count <- 7
+step_size <- 1000
+length_dataset <- dim(read_csv(input_file, skip_empty_rows = T, skip = 6))[1]
 
-raw <- read_csv(input_file, skip_empty_rows = T, skip = 6)
+data <- data.frame(
+  X1 = c(), X2 = c(), X3 = c(), X4 = c(), X5 = c(), X6 = c(), X7 = c()
+)
+
+for (i in 0:(length_dataset/step_size)) {
+  
+  tmp <- read_csv(input_file, skip_empty_rows = T, skip = skip_count + i * step_size, n_max = step_size, col_names = FALSE)
+  
+  tmp <- tmp %>% 
+    arrange("X1", "X5") %>% 
+    noop()
+  
+  tmp %>% 
+    mutate("X6" = str_remove_all("X6", c("\\[") ) ) %>%     # remove list symbols
+    mutate("X6" = str_remove_all("X6", c("\\]") ) ) %>% 
+    rowwise() %>%                                                 # turn list into rows (help index creation)
+    mutate("X7" = paste(1:"X2", collapse = " ")) %>% 
+    separate_rows("X6", "X7", sep = " ", convert = T) %>% 
+    noop() -> tmp
+  
+  data %>%
+    bind_rows(tmp) -> data
+}
+
+names(data) <- c("run_number", "agent_count", "epsilon", "backfire", "step", "list_op", "agent")
+
+
+# ------------- #
+
+raw <- read_csv(input_file, skip_empty_rows = T, skip = 6, n_max = 10000)
 
 names(raw) <- c("run_number", "agent_count", "epsilon", "backfire", "step", "list_op")
 
